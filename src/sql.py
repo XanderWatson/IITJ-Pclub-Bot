1c3c1ad5dae7fce6baed7a06428b18deba9d58bb
manav1403 <manavavni@gmail.com>
2020-11-28 00:17:45 +0530

    restructured

--- src/sql.py
+++ src/sql.py
@@ -5,58 +5,74 @@ import sqlite3
 def clean(string):
     return string.replace("'", '').replace('"', '')
 
+class Column:
+    def __init__(self, name, type , primary_key = False, not_null = True):
+        self.name = name
+        self.type = type
+        self.primary_key = primary_key
+        self.not_null = not_null
+    
+    def __str__(self):
+        out = f'''{self.name} {self.type}'''
+        if self.primary_key: out +=  " PRIMARY KEY"
+        if self.not_null: out += " NOT NULL"
+        return out
 
-class Table:
 
-    def __init__(self, table, columns: list):
-        self.conn = sqlite3.connect('data.db')
+class Database:
+
+    def __init__(self, database: str):
+        self.conn = sqlite3.connect(database)
         self.c = self.conn.cursor()
 
-        command = f'''CREATE TABLE {table}
-         ({columns[0][0]} {columns[0][1]} PRIMARY KEY NOT NULL'''
 
-        for i in range(1, len(columns)):
-            column, data_type = columns[i]
-            command += f", {column} {data_type} NOT NULL"
-        command += ");"
-
-        self.c.execute(command)
-
-        self.table = table
+class Table:
+    def __init__(self,database, name , columns: list):
+        self.name = name
         self.columns = columns
-
+        self.db = database
+        try:
+            self.db.c.execute(f'''SELECT * FROM {self.name}''')
+        except:
+            print(self.get_def())
+            self.db.c.execute(f'''CREATE TABLE {self.name} ( {self.get_def()})''')
+    
+    def get_columns(self):
+        return [col.name for col in self.columns]
+    
+    def get_def(self):
+        return ", ".join(map(lambda col: str(col), self.columns))
+    
     def get(self, id_str, columns: str = '*'):
 
-        self.c.execute(
+        self.db.c.execute(
             clean(
                 "SELECT {0} FROM {1} WHERE id=:id".format(
-                    columns, self.table), {'id': id_str}))
-        return self.c.fetchone()
+                    columns, self.name)), {'id': id_str})
+        return self.db.c.fetchone()
 
     def get_all(self, columns: str = '*'):
-        self.c.execute(
+        self.db.c.execute(
             clean(
-                "SELECT {0} FROM {1}".format(columns, self.table))
+                "SELECT {0} FROM {1}".format(columns, self.name))
         )
-        return self.c.fetchall()
+        return self.db.c.fetchall()
 
     def add_element(self, id_str, values: dict = None):
         if values is None:
             values = {}
 
         values['id'] = id_str
-        n_col = []
         for column in self.columns:
-            n_col.append(column[0])
-            if column[0] not in values:
-                values[column[0]] = 0  # sets default value 0
+            if column.name not in values:
+                values[column.name] = 0  # sets default value 0
 
-        with self.conn:
-            self.c.execute(
+        with self.db.conn:
+            self.db.c.execute(
                 clean(
                     "INSERT INTO {0} VALUES {1}".format(
-                        self.table,
-                        tuple(map(lambda col: ':' + col, n_col))
+                        self.name,
+                        tuple(map(lambda col: ':' + col, self.get_columns()))
                     )),
                 values
             )
@@ -65,12 +81,12 @@ class Table:
     def update(self, id_str, values: dict):
         values['id'] = id_str
 
-        with self.conn:
-            self.c.execute(
+        with self.db.conn:
+            self.db.c.execute(
                 clean(
                     "UPDATE {0} SET {1} WHERE id=:id".format(
-                        self.table,
-                        tuple(map(lambda col: col + ' = :' + col, values))
+                        self.name,
+                        ", ".join(map(lambda col: col + ' = :' + col, values))
                     )),
                 values
             )
@@ -78,8 +94,20 @@ class Table:
 
     def delete(self, id_str):
 
-        with self.conn:
-            self.c.execute(
+        with self.db.conn:
+            self.db.c.execute(
                 clean(
-                    f"DELETE from {self.table} where id = {id_str};"
+                    f"DELETE from {self.name} where id = {id_str};"
                 ))
+
+
+db = Database('data.db')
+user = Table(db, 'user',[
+    Column('id', 'INTEGER'),
+    Column('name', 'STRING')
+])
+# user.add_element(1,{'name': "manav"})
+#print(user.get_all())
+#print(user.get(1,"name"))
+#user.update(1,{'name': "manav kapoor"})
+#user.delete(1)
